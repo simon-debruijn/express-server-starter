0a7b99901b621f0b55e41fd7879166ae4753b06b
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-16 15:37:18 +0200

    Remove patch

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -6,4 +6,3 @@ export const usersRouter = Router();
 usersRouter.get('/', usersController.getUsers);
 usersRouter.post('/register', usersController.register);
 usersRouter.post('/login', usersController.login);
-usersRouter.patch('/:email', usersController.changeUserByEmail);
