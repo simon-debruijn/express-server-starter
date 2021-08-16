18336b577afcbd662726f845d1956e8af8ff32a9
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-16 14:38:22 +0200

    Use helmet middleware

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import { helloRouter } from './hello/hello.router';
 import { handleExceptions } from './exceptions/handleExceptions.middleware';
 import { handleServerErrors } from './errors/handleServerErrors.middleware';
 import { usersRouter } from './users/users.router';
+import helmet from 'helmet';
 
 dotenv.config();
 
 export const app = express();
 
 app.use(express.json());
+app.use(helmet());
 
 app.use('/hello', helloRouter);
 app.use('/users', usersRouter);
