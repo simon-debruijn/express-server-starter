b74dfedb5b25a708aa61e5a8ce69c73548ab4f66
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-17 16:16:03 +0200

    Use cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import compression from 'compression';
+import cors from 'cors';
 
 import { helloRouter } from './hello/hello.router';
 import { usersRouter } from './users/users.router';
 
 import { handleExceptions } from './exceptions/handleExceptions.middleware';
 import { handleServerErrors } from './errors/handleServerErrors.middleware';
+
+import config from '../config.json';
 
 dotenv.config();
 
@@ -16,6 +19,7 @@ export const app = express();
 app.use(compression());
 app.use(express.json());
 app.use(helmet());
+app.use(cors({ origin: config.origins }));
 
 app.use('/hello', helloRouter);
 app.use('/users', usersRouter);
