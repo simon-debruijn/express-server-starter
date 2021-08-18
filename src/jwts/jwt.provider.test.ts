509066fa84c172cfa9ce638b515992c95b02c1f3
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-18 17:00:45 +0200

    Add tests for jwts

---
+++ src/jwts/jwt.provider.test.ts
@@ -0,0 +1,45 @@
+import { JsonWebTokenError } from 'jsonwebtoken';
+import * as jwtprovider from './jwt.provider';
+
+describe('jwt.provider', () => {
+  const correctPayload = { email: 'test@test.com' };
+  const secret = 'shhhhhh';
+  const wrongSecret = 'hssssss';
+
+  test('sign should return a token when the correct data is passed as payload', async () => {
+    const token = await jwtprovider.sign(correctPayload, secret, {
+      algorithm: 'HS256',
+      expiresIn: '1d',
+    });
+    expect(token).toBeDefined();
+    expect(token.length).toBeGreaterThan(0);
+  });
+
+  test('verify should verify and return email when the correct token and the correct secret are passed', async () => {
+    const token = await jwtprovider.sign(correctPayload, secret, {
+      algorithm: 'HS256',
+      expiresIn: '1d',
+    });
+
+    const { exp, iat, ...decoded } = await jwtprovider.verify(token, secret, {
+      algorithms: ['HS256'],
+    });
+
+    expect(decoded).toEqual(correctPayload);
+  });
+
+  test('verify should throw an error when the wrong secret is passed', async () => {
+    const token = await jwtprovider.sign(correctPayload, secret, {
+      algorithm: 'HS256',
+      expiresIn: '1d',
+    });
+
+    try {
+      await jwtprovider.verify(token, wrongSecret, {
+        algorithms: ['HS256'],
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(JsonWebTokenError);
+    }
+  });
+});
