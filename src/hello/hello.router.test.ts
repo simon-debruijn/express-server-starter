5000637200b1f200d9c8584e39991c2299b8e815
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-18 17:16:32 +0200

    Fix unimplemented test

--- src/hello/hello.router.test.ts
+++ src/hello/hello.router.test.ts
@@ -18,12 +18,20 @@ describe('hello.router', () => {
   });
 
   test("getForbidden returns 'You were granted access' when authorized", async () => {
-    const response = await request(app).get('/hello/forbidden');
-    expect(response.body).toEqual({
-      message: 'Was unable to authenticate',
-      name: 'UNAUTHORIZED',
-      status: 401,
-      type: 'HTTP_EXCEPTION',
-    });
+    const email = 'test@test.be';
+    const password = 'HJGDYGYjhkjh';
+
+    const responseRegister = await request(app)
+      .post('/users/register')
+      .set('Content-Type', 'application/json')
+      .send({ email, password });
+
+    expect(responseRegister.body.token).toBeDefined();
+
+    const responseForbidden = await request(app)
+      .get('/hello/forbidden')
+      .set('Authorization', `Bearer ${responseRegister.body.token}`);
+
+    expect(responseForbidden.text).toEqual('You were granted access');
   });
 });
