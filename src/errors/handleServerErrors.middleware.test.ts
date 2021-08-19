db132a51bc9d2926d21b0dae2663619394cbf061
Simon de Bruijn <simon.debruijn@publiq.be>
2021-08-19 16:47:33 +0200

    Add tests for handleServerErrors.middleware

---
+++ src/errors/handleServerErrors.middleware.test.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { handleServerErrors } from './handleServerErrors.middleware';
+
+describe('handleServerErrors.middleware', () => {
+  let request: Request, response: Response, next: NextFunction;
+
+  beforeEach(() => {
+    request = {} as unknown as Request;
+    response = {
+      status: jest.fn(() => response),
+      send: jest.fn(),
+    } as unknown as Response;
+    next = jest.fn();
+    console.error = jest.fn();
+  });
+  test('handleServerErrors should send a response if an error is passed', () => {
+    const error = new Error('Server error');
+    handleServerErrors(error, request, response, next);
+
+    expect(console.error).toBeCalledTimes(1);
+    expect(response.status).toBeCalledWith(500);
+    expect(response.send).toBeCalledWith('Internal server error');
+  });
+});
