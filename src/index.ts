2b8c3dd3121f12662c8cfc922fd0972cd2a091aa
Simon de Bruijn <simon.debruijn@publiq.be>
2022-03-11 11:25:32 +0100

    Add log of threads for deployment

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { app } from './app';
 import { PORT } from './constants';
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT} with ${process.env.UV_THREADPOOL_SIZE} threads`,
+  );
 });
