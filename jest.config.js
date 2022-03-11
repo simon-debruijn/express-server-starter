4c836cf3f3b8abb54a71dd844324fb97a0ebc6e6
Simon de Bruijn <simon.debruijn@publiq.be>
2022-03-11 14:36:15 +0100

    Add path aliases to jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+
   testEnvironment: 'node',
   testPathIgnorePatterns: ['dist'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
