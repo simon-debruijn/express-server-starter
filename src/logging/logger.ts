b1ea7b5d65bf626ba5701f2a34c32a236ec66256
Simon de Bruijn <simon.debruijn@publiq.be>
2022-03-14 16:39:24 +0100

    Add logging to file for production builds

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,5 +1,26 @@
 import pino from 'pino';
 
-const logger = pino();
+import path from 'path';
+
+const file = path.join(__dirname, `../../logs/pino-${process.pid}-example`);
+
+const transport = pino.transport({
+  targets: [
+    {
+      level: 'info',
+      target: 'pino/file',
+      options: {
+        destination: file,
+        mkdir: true,
+      },
+    },
+  ],
+});
+
+const devOptions = {};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const logger = pino(isProduction ? transport : devOptions);
 
 export { logger };
